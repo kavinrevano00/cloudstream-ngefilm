61a7b3ffac2441c2d57088c9c06cac652dde4bdc
kavinrevano00 <kavinrevano00@gmail.com>
2026-08-28 10:43:18 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,19 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+rootProject.name = "CloudstreamPlugins"
+
+// This file sets what projects are included.
+// All new projects should get automatically included unless specified in the "disabled" variable.
+
+val disabled = listOf<String>()
+
+File(rootDir, ".").eachDir { dir ->
+    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        include(dir.name)
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-    google()
-    mavenCentral()
-    maven("https://jitpack.io")
-}
-    }
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
 
-rootProject.name = "cloudstream-ngefilm-repo"
-
-file(".").listFiles()?.filter { it.isDirectory && File(it, "build.gradle.kts").exists() }?.forEach {
-    include(":${it.name}")
-}
+// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
+// include("PluginName")
